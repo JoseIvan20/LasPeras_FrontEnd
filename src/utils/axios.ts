3a964ad51de7c436aaceb7379fa0932781544be2
JoseIvan20 <ivanjg1029@gmail.com>
2024-12-22 17:58:24 -0600

    Finalized

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const apiSendMail = axios.create({
-  baseURL: import.meta.env.VITE_API_CONTACT
+  // baseURL: import.meta.env.VITE_API_CONTACT
+  baseURL: 'https://jazzy-faun-44d6d7.netlify.app/.netlify/functions'
 })
 
 export default apiSendMail
