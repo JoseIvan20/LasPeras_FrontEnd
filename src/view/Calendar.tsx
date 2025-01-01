e39cdc25304cedd6e07a682175308aec36bdaff0
JoseIvan20 <ivanjg1029@gmail.com>
2025-01-01 15:46:33 -0600

    Changes Sidebar, component calendar imcomplete v3

--- src/view/Calendar.tsx
+++ src/view/Calendar.tsx
@@ -1,18 +1,6 @@
-import { CornerUpLeft, TriangleAlert } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
 import CalendarLayout from '../components/calendar/CalendarLayout'
 
 const Calendar = () => {
-
-  const navigate = useNavigate()
-
-  const handleGoBack = () => {
-    if (window.history.length > 1) {
-      navigate(-1) // Con esto el usuario puede regresar a la ventana en la que ventana antes de haber navegado a otra
-    } else {
-      navigate('/dashboard')
-    }
-  }
 
   return (
     <div className='flex flex-col items-center'>
